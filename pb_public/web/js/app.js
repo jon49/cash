ba5343e41d2dbf07913a214db137ce1c4725a339
Jon Nyman <nymanjon@gmail.com>
2024-12-19 21:37:56 -0700

    Remove progressive enhancement for toaster. I couldn't get the CSS to work with PicoCSS.

--- pb_public/web/js/app.js
+++ pb_public/web/js/app.js
@@ -14,40 +14,17 @@ function connectedCallback(ctx) {
 }
 
 customElements.define("date-fill",
-class DateFill extends HTMLElement {
-    connectedCallback() {
-        connectedCallback(this)
-    }
+    class DateFill extends HTMLElement {
+        connectedCallback() {
+            connectedCallback(this)
+        }
 
-    init() {
-        let el = this.firstElementChild
-        let value = el?.value
-        if (value?.length > 0) return
-        let today = new Date()
-        let formattedDate = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
-        el.value = formattedDate;
-    }
-})
-
-function togglePopover(el) {
-    let wordCount = el.textContent.split(/\s+/).length
-    let timeout = 1e3 + wordCount * 400
-    setTimeout(() => {
-        el.parentElement.remove()
-    }, timeout)
-}
-
-customElements.define("noscript-toast", class NoScriptToast extends HTMLElement {
-    connectedCallback() {
-        let d = document
-        let el = d.body.querySelector("noscript")
-        if (!el) return
-        let html = el.textContent.trim()
-        if (!html) return
-        let toaster = d.createElement("article")
-        toaster.setAttribute("role", "alert")
-        toaster.innerHTML = html
-        this.append(toaster)
-        togglePopover(toaster)
-    }
-})
+        init() {
+            let el = this.firstElementChild
+            let value = el?.value
+            if (value?.length > 0) return
+            let today = new Date()
+            let formattedDate = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
+            el.value = formattedDate;
+        }
+    })
