b62978d5aea8855a270d7db7d2341f6c1982ab28
Jon Nyman <nymanjon@gmail.com>
2024-12-19 23:08:08 -0700

    Improve rendering of transaction amounts.

--- pb_hooks/transactions.pb.js
+++ pb_hooks/transactions.pb.js
@@ -10,27 +10,32 @@ routerAdd("get", "/app/transactions", e => {
 
     let categories = $app.findRecordsByFilter("categories", `user='${userId}'`)
 
+    function formatAmountView(amount) {
+        return amount < 0 ? `($${Math.abs(amount).toFixed(2)})` : `$${amount.toFixed(2)}`
+    }
     let { capitalize, formatDate } = require(`${__hooks}/utils.js`)
     let data = {
         transactions: transactions.map(transaction => {
             let date = formatDate(transaction.get("date"))
             let category = categories.find(category => category.id === transaction.get("category"))
             let categoryName = `${capitalize(category.get("category_type"))} - ${category.get("name")}`
+            let amount = categoryName.startsWith("Income") ? transaction.get("amount") : -transaction.get("amount")
             return {
                 id: transaction.id,
                 date,
                 description: transaction.get("description"),
-                amount: -transaction.get("amount"),
+                amount,
+                amountView: formatAmountView(amount),
                 category: categoryName,
             }
         }),
     }
 
-    data.total = data.transactions.reduce((sum, transaction) => {
+    data.total = formatAmountView(data.transactions.reduce((sum, transaction) => {
         return (transaction.category.startsWith("Income"))
-            ? sum - transaction.amount
+            ? sum + transaction.amount
             : sum + transaction.amount
-    }, 0)
+    }, 0))
 
     const html = $template.loadFiles(
         `${__hooks}/pages/layout.html`,
