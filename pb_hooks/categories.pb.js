aff69283377e0fdf01770054c27924c7dab09d5b
Jon Nyman <nymanjon@gmail.com>
2024-12-20 18:21:37 -0700

    Added ability to delete categories.

--- pb_hooks/categories.pb.js
+++ pb_hooks/categories.pb.js
@@ -6,18 +6,62 @@ routerAdd("get", "/app/categories", e => {
         return e.redirect(302, "/app/categories/edit")
     }
 
-    let {capitalize} = require(`${__hooks}/utils.js`)
+    let { capitalize } = require(`${__hooks}/utils.js`)
 
-    let data = records.map(x => ({
+    let categories = records.map(x => ({
         category: capitalize(x.get("category_type")),
         subcategory: x.get("name"),
         id: x.id,
+        deleted: x.get("deleted").toString(),
     }))
+
+    let data = {
+        categories: categories.filter(x => x.deleted === ""),
+        deletedCategories: categories.filter(x => x.deleted !== ""),
+    }
+    data.hasDeleted = data.deletedCategories.length > 0
+    data.hasCategories = data.categories.length > 0
 
     const html = $template.loadFiles(
         `${__hooks}/pages/layout.html`,
         `${__hooks}/pages/categories.html`,
-    ).render({categories: data})
+    ).render(data)
 
     return e.html(200, html)
+})
+
+routerAdd("post", "/app/categories/delete", e => {
+    // Delete
+    let { id } = e.requestInfo().body
+    if (!id) {
+        return e.redirect(302, `/app/categories?msg=Category not found!`)
+    }
+
+    let record = $app.findRecordById("categories", id)
+    if (!record) {
+        return e.redirect(302, `/app/categories?msg=Category not found!`)
+    }
+    record.set("deleted", new Date().toISOString())
+    console.log("RECORD::::::::::::::::::::", record)
+
+    $app.save(record)
+
+    return e.redirect(303, `/app/categories`)
+})
+
+routerAdd("post", "/app/categories/restore", e => {
+    // Delete
+    let { id } = e.requestInfo().body
+    if (!id) {
+        return e.redirect(302, `/app/categories?msg=Category not found!`)
+    }
+
+    let record = $app.findRecordById("categories", id)
+    if (!record) {
+        return e.redirect(302, `/app/categories?msg=Category not found!`)
+    }
+    record.set("deleted", "")
+    $app.save(record)
+
+    return e.redirect(303, `/app/categories`)
 })
