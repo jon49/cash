e19bb32ffcace1e4441ff63e2df0a371b3c3d2ca
Jon Nyman <nymanjon@gmail.com>
2024-12-22 21:13:20 -0700

    Code clean up.

--- pb_hooks/transactions_edit.pb.js
+++ pb_hooks/transactions_edit.pb.js
@@ -52,8 +52,6 @@ routerAdd("get", "/app/transactions/edit/", e => {
     return e.html(200, html)
 })
 
-/// <reference path="../pb_data/types.d.ts" />
-
 routerAdd("post", "/app/transactions/edit/", e => {
     let { id, date, description, categoryId, amount } = e.requestInfo().body
     let userId = e.get("userId")
