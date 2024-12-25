702a63904f6c249338d8e51e2f7a84a2ddde63ce
Jon Nyman <nymanjon@gmail.com>
2024-12-25 16:21:07 -0700

    Implemented delete all data before running tests.

---
+++ pb_hooks/delete-all-data.pb.js
@@ -0,0 +1,23 @@
+/// <reference path="../pb_data/types.d.ts" />
+
+routerAdd("post", "/app/delete-all-data/", e => {
+    let userId = e.get("userId")
+
+    let transactions = $app.findRecordsByFilter("transactions", `user='${userId}'`)
+    let categories = $app.findRecordsByFilter("categories", `user='${userId}'`)
+    let sessions = $app.findRecordsByFilter("sessions", `user='${userId}'`)
+
+    for (let transaction of transactions) {
+        $app.delete(transaction)
+    }
+
+    for (let category of categories) {
+        $app.delete(category)
+    }
+
+    for (let session of sessions) {
+        $app.delete(session)
+    }
+
+    return e.json(200, { success: true })
+})
