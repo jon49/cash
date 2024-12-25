1b3d9a5faf7193f3080760eb5a54c7466cdeaaa7
Jon Nyman <nymanjon@gmail.com>
2024-12-25 16:21:46 -0700

    Added tests for going to the application without logging in. Fixed code.

--- pb_hooks/auth_middleware.pb.js
+++ pb_hooks/auth_middleware.pb.js
@@ -4,12 +4,16 @@ routerUse(e => {
     if (!e.request.url.path.startsWith("/app")) {
         return e.next()
     }
-    let sessionCookie = e.request.cookie("session")
+    let sessionCookie = e.request.cookies().find(cookie => cookie.name === "session")
     let session = sessionCookie?.value
     if (!session) return e.redirect(302, "/login")
 
-    let sessionRecord = $app.findRecordById("sessions", session)
-    if (!sessionRecord) return e.redirect(302, "/login")
+    let sessionRecord
+    try {
+        sessionRecord = $app.findRecordById("sessions", session)
+    } catch (_) {
+        return e.redirect(302, "/login")
+    }
 
     e.set("userId", sessionRecord.get("user"))
     e.next()
