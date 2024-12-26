efb9c5dd5ed83b00f1d96455721b574256f4394e
Jon Nyman <nymanjon@gmail.com>
2024-12-25 20:53:05 -0700

    Make cookie file a tsv extension instead of txt.

--- tests/test-all.sh
+++ tests/test-all.sh
@@ -30,12 +30,12 @@ run_hurl_tests() {
 cd "$(dirname "$0")"
 
 # Find all .hurl files and run tests
-run_hurl_tests ./login.hurl --cookie-jar ./cookie-jar.txt
-run_hurl_tests ./clean-db.hurl --cookie ./cookie-jar.txt
+run_hurl_tests ./login.hurl --cookie-jar ./cookie-jar.tsv
+run_hurl_tests ./clean-db.hurl --cookie ./cookie-jar.tsv
 run_hurl_tests ./logged-in-redirects.tests.hurl &
 run_hurl_tests ./login-page.tests.hurl &
-run_hurl_tests ./login.hurl --cookie-jar ./cookie-jar.txt
-run_hurl_tests ./create.tests.hurl --cookie ./cookie-jar.txt
-run_hurl_tests ./delete.tests.hurl --cookie ./cookie-jar.txt
+run_hurl_tests ./login.hurl --cookie-jar ./cookie-jar.tsv
+run_hurl_tests ./create.tests.hurl --cookie ./cookie-jar.tsv
+run_hurl_tests ./delete.tests.hurl --cookie ./cookie-jar.tsv
 
 cd -
