8bf649d12b6edbbf5859f1a90f031f5ff7ecdb0c
Jon Nyman <nymanjon@gmail.com>
2024-12-27 22:48:54 -0700

    Update build script to run through all html files to add hash to static files.

--- tasks/build.sh
+++ tasks/build.sh
@@ -14,11 +14,7 @@ generate_md5_hash() {
 }
 
 # Read layout.html
-layout_file="pb_hooks/pages/layout.html"
 manifest_file="dist/public/web/manifest.json"
-dist_layout_file="dist/pb_hooks/pages/layout.html"
-cp $layout_file $dist_layout_file
-
 destStaticDir="dist/public"
 
 # In the dist/public/web/manifest.json file, find linked static image src files and add MD5 hash
@@ -34,20 +30,23 @@ while IFS= read -r line; do
 done < $manifest_file
 
 # Find linked static files and update layout.html with MD5 hash
-while IFS= read -r line; do
-    if [[ $line =~ \<link.*href=\"([^\"]+)\" ]]; then
-        file="${BASH_REMATCH[1]}"
-        if [[ $file != http* ]]; then
-            hash=$(generate_md5_hash "$destStaticDir/$file")
-            new_file="${file}?_=${hash}"
-            sed -i "s|$file|$new_file|g" $dist_layout_file
+for dist_html_file in dist/pb_hooks/pages/*.html; do
+    echo "Updating $dist_html_file"
+    while IFS= read -r line; do
+        if [[ $line =~ \<link.*href=\"([^\"]+)\" ]]; then
+            file="${BASH_REMATCH[1]}"
+            if [[ $file != http* ]]; then
+                hash=$(generate_md5_hash "$destStaticDir/$file")
+                new_file="${file}?_=${hash}"
+                sed -i "s|$file|$new_file|g" $dist_html_file
+            fi
+        elif [[ $line =~ \<.*src=\"([^ ]*)\" ]]; then
+            file="${BASH_REMATCH[1]}"
+            if [[ $file != http* ]]; then
+                hash=$(generate_md5_hash "pb_public/$file")
+                new_file="${file}?_=${hash}"
+                sed -i "s|$file|$new_file|g" $dist_html_file
+            fi
         fi
-    elif [[ $line =~ \<.*src=\"([^ ]*)\" ]]; then
-        file="${BASH_REMATCH[1]}"
-        if [[ $file != http* ]]; then
-            hash=$(generate_md5_hash "pb_public/$file")
-            new_file="${file}?_=${hash}"
-            sed -i "s|$file|$new_file|g" $dist_layout_file
-        fi
-    fi
-done < $layout_file
+    done < $dist_html_file
+done
