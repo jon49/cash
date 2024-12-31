bb3810bd52ec0223571830fea096b2dfd51c1c2d
Jon Nyman <nymanjon@gmail.com>
2024-12-31 09:33:31 -0700

    Updated test file to be more in line with how Hurl tests are supposed to be performed.

--- tests/test-all.sh
+++ tests/test-all.sh
@@ -22,33 +22,34 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-# Function to run hurl tests
-run_hurl_tests() {
-    local test_file=$1
-    if $DEBUG_MODE; then
-        echo "Running $test_file in debug mode…"
-        echo "hurl --verbose \"$test_file\" --variables-file \"$env.env\" \"${@:2}\""
-        hurl --verbose "$test_file" --variables-file "$env.env" "${@:2}"
-    else
-        echo "Running $test_file…"
-        echo "hurl \"$test_file\" --variables-file \"$env.env\" \"${@:2}\" --no-output"
-        hurl "$test_file" --variables-file "$env.env" "${@:2}" --no-output
-    fi
-}
-
 # cd into where current file exists
 cd "$(dirname "$0")"
 
-# Find all .hurl files and run tests
-run_hurl_tests ./login.hurl --cookie-jar ./cookie-jar.tsv
-run_hurl_tests ./clean-db.hurl --cookie ./cookie-jar.tsv
-run_hurl_tests ./login-page.tests.hurl &
-run_hurl_tests ./not-logged-in-redirects.tests.hurl &
-run_hurl_tests ./login.hurl --cookie-jar ./cookie-jar.tsv
-run_hurl_tests ./category.tests.hurl --cookie ./cookie-jar.tsv
-run_hurl_tests ./transaction.tests.hurl --cookie ./cookie-jar.tsv
-run_hurl_tests ./transaction-htmf.tests.hurl --cookie ./cookie-jar.tsv
-run_hurl_tests ./export.tests.hurl --cookie ./cookie-jar.tsv
-run_hurl_tests ./logout.tests.hurl --cookie ./cookie-jar.tsv
+login() {
+    hurl --test --variables-file "$env.env" --cookie-jar ./cookie-jar.tsv ./login.hurl 
+}
+
+run_with_cookies() {
+    # If debug mode then add argument "--error-format long"
+    if [ "$DEBUG_MODE" = true ]; then
+        hurl --test --jobs 1 --variables-file "$env.env" --cookie ./cookie-jar.tsv \
+            --error-format long "${@:1}"
+    else
+        hurl --test --jobs 1 --variables-file "$env.env" --cookie ./cookie-jar.tsv "${@:1}"
+    fi
+}
+
+login
+run_with_cookies \
+    ./clean-db.hurl \
+    ./login-page.tests.hurl \
+    ./not-logged-in-redirects.tests.hurl
+login
+run_with_cookies \
+    ./category.tests.hurl \
+    ./transaction.tests.hurl \
+    ./transaction-htmf.tests.hurl \
+    ./export.tests.hurl \
+    ./logout.tests.hurl
 
 cd -
