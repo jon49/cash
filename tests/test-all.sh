70c8973b04f11fcf8f89df4f2afa17667c5474ef
Jon Nyman <nymanjon@gmail.com>
2024-12-31 08:51:57 -0700

    Added transaction htmf tests.

--- tests/test-all.sh
+++ tests/test-all.sh
@@ -45,6 +45,7 @@ run_hurl_tests ./not-logged-in-redirects.tests.hurl &
 run_hurl_tests ./login.hurl --cookie-jar ./cookie-jar.tsv
 run_hurl_tests ./category.tests.hurl --cookie ./cookie-jar.tsv
 run_hurl_tests ./transaction.tests.hurl --cookie ./cookie-jar.tsv
+run_hurl_tests ./transaction-htmf.tests.hurl --cookie ./cookie-jar.tsv
 run_hurl_tests ./export.tests.hurl --cookie ./cookie-jar.tsv
 
 cd -
