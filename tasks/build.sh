f6e1d3bc1096d8df64c4df4559a1088b93643b81
Jon Nyman <nymanjon@gmail.com>
2025-01-03 12:04:26 -0700

    Update CSS to reference PicoCSS so my CSS can override Pico without resorting to anti-patterns.

--- tasks/build.sh
+++ tasks/build.sh
@@ -21,6 +21,20 @@ generate_md5_hash() {
 manifest_file="dist/public/web/manifest.json"
 destStaticDir="dist/public"
 
+# In the app.css file find linked static image src files and add MD5 hash
+app_css="dist/public/web/css/app.css"
+while IFS= read -r line; do
+    # Check if the line contains a URL pattern
+    if [[ $line =~ url\(\"([^\"]+)\"\) ]]; then
+        file="${BASH_REMATCH[1]}"
+        if [[ $file != http* ]]; then
+            hash=$(generate_md5_hash "$destStaticDir/$file")
+            new_file="${file}?_=${hash}"
+            sed -i "s|$file|$new_file|g" $app_css
+        fi
+    fi
+done < $app_css
+
 # In the dist/public/web/manifest.json file, find linked static image src files and add MD5 hash
 while IFS= read -r line; do
     if [[ $line =~ \"src\":\ \"([^\"]+)\" ]]; then
