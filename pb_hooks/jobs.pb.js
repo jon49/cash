057a61c0cf3607691982fb631bc05edbdf5f37a3
Jon Nyman <nymanjon@gmail.com>
2025-02-24 21:03:30 -0700

    Fix cron jobs.

--- pb_hooks/jobs.pb.js
+++ pb_hooks/jobs.pb.js
@@ -2,9 +2,10 @@
 
 // Midnight cron job to remove deleted categories
 cronAdd("removeDeletedCategories", "0 0 * * *", () => {
-    let now = new Date()
-    let last24hours = now.setDate(now.getDate() - 1).toJSON().replace('T', ' ')
-    let categories = $app.findRecordsByFilter("categories", `deleted < '${last24hours}'`)
+    var now = new Date()
+    var yesterday = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1)
+    let last24hours = yesterday.toJSON().replace('T', ' ')
+    let categories = $app.findRecordsByFilter("categories", `deleted != '' && deleted < '${last24hours}'`)
 
     for (let category of categories) {
         $app.delete(category)
@@ -13,9 +14,10 @@ cronAdd("removeDeletedCategories", "0 0 * * *", () => {
 
 // Midnight cron job to remove deleted transactions
 cronAdd("removeDeletedTransactions", "0 0 * * *", () => {
-    let now = new Date()
-    let last24hours = now.setDate(now.getDate() - 1).toJSON().replace('T', ' ')
-    let transactions = $app.findRecordsByFilter("transactions", `deleted < '${last24hours}'`)
+    var now = new Date()
+    var yesterday = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1)
+    let last24hours = yesterday.toJSON().replace('T', ' ')
+    let transactions = $app.findRecordsByFilter("transactions", `deleted != '' && deleted < '${last24hours}'`)
 
     for (let transaction of transactions) {
         $app.delete(transaction)
